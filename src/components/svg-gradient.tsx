05d972a8c9e0990168568ccf330966f5d28d39d2
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-06-21 13:32:07 +0200

    improve gradient support

--- src/components/svg-gradient.tsx
+++ src/components/svg-gradient.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
 export const SvgGradient = component$(() => {
-  return <svg hidden aria-hidden="true" focusable="false">
+  return <svg style="position:fixed; top: -1000px; opacity: 0; pointer-event: none" aria-hidden="true" focusable="false">
     <linearGradient id="svg-gradient">
       <stop offset="0%" stop-color="var(--gradient-start)" />
       <stop offset="100%" stop-color="var(--gradient-end)" />
--- src/root.tsx
+++ src/root.tsx
@@ -1,52 +1,32 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { useHueEyeProvider } from "./components/hue/hue";
-
-
-export const HueProvider = component$(() => {
-  useHueEyeProvider();
-  useStyles$(`
-    html {
-      --hue: 0;
-    }
-    body {
-      display: flex;
-      flex-direction: column;
-      gap: var(--size-2);
-      padding: var(--size-2);
-      box-sizing: border-box;
-    }
-    header, footer {
-      background-color: var(--surface-low);
-    }
-    main {
-      background-color: var(--surface);
-      flex: 1;
-      padding: var(--size-2);
-      box-sizing: border-box;
-    }
-  `);
-  return <Slot/>;
-})
+import { HueEyeProvider } from "./components/hue/hue";
+import './root.scss';
 
 export default () => {
 
   return (
-    <HueProvider>
+    <>
       <head>
         <meta charSet="utf-8" />
         <title>Qwik Blank App</title>
       </head>
       <body>
+        <HueEyeProvider>
         <header>
           <h1>HueEye</h1>
         </header>
         <main>
           <button class="btn-fill primary">Primary button</button>
+          <button class="btn-icon gradient">
+            <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+              <circle cx="12" cy="12" r="10"/>
+            </svg>
+          </button>
         </main>
         <footer>
           <p>This is a random footer</p>
         </footer>
+        </HueEyeProvider>
       </body>
-    </HueProvider>
+    </>
   );
 };
