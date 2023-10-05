866a238cf495633799cc79a72749e87e97e09f48
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-10-05 10:27:58 +0200

    improve dialog&tooltip

--- src/routes/tooltip/index.tsx
+++ src/routes/tooltip/index.tsx
@@ -4,16 +4,16 @@ export default component$(() => {
   return <section id="tooltip-page" aria-labelledby="tooltip-title">
     <h1 id="tooltip-title">Tooltip</h1>
     <article>
-      <button class="btn tooltip-top" aria-label="This is a tooltip">
+      <button class="btn tooltip-top" aria-description="This is a tooltip">
         Top
       </button>
-      <button class="btn tooltip-right" aria-label="This is a tooltip">
+      <button class="btn tooltip-right" aria-description="This is a tooltip">
         Right
       </button>
-      <button class="btn tooltip-bottom" aria-label="This is a tooltip">
+      <button class="btn tooltip-bottom" aria-description="This is a tooltip">
         Bottom
       </button>
-      <button class="btn tooltip-left" aria-label="This is a tooltip">
+      <button class="btn tooltip-left" aria-description="This is a tooltip">
         Left
       </button>
     </article>
