b37a42c0e05563ca13e14e1f733efffbe9d345eb
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-08-26 10:01:33 +0200

    do not include prefetch service worke

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -17,17 +17,20 @@ import Root from './root';
 export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
-    prefetchStrategy: {
-      implementation: {
-        linkInsert: 'html-append',
-        linkRel: 'modulepreload'
-      }
-    },
     ...opts,
     // Use container attributes to set attributes on the html tag.
     containerAttributes: {
       lang: 'en-us',
       ...opts.containerAttributes,
     },
+    prefetchStrategy: {
+      implementation: {
+        linkInsert: 'html-append',
+        linkRel: 'modulepreload'
+      }
+    },
+    qwikPrefetchServiceWorker: {
+      include: false,
+    }
   });
 }
